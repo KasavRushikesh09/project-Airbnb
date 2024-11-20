516c070ed1b7a05c770a6558b721df58d41a73e3
Rushikesh Kasav <rishikeshkasav2003@gmail.com>
2024-11-20 12:14:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,26 +35,26 @@ app.get("/", (req, res) => {
   res.send("Hi, I am rushi kasav");
 });
 
-// const validateListing = (req, res, next) => {
-//   let { error } = listingSchema.validate(req.body);
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
 
-//   if (error) {
-//     let errMsg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(400, errMsg);
-//   } else {
-//     next();
-//   }
-// };
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
-// const validateReview = (req, res, next) => {
-//   let { error } = reviewSchema.validate(req.body);
-//   if (error) {
-//     let errMsg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(400, errMsg);
-//   } else {
-//     next();
-//   }
-// };
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 // // Index Route
 
 app.get(
